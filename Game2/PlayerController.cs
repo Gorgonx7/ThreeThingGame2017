1685eaee20acd7b50b0e1a808a5c450386b03ac2
Gorgonx7 <james.gordon1628@gmail.com>
2017-11-03 17:41:59 +0000

    Created PlayerController

--- Game2/PlayerController.cs
+++ Game2/PlayerController.cs
@@ -9,32 +9,142 @@ namespace Game2
 {
     class PlayerController
     {
-        GamePadCapabilities capabilities;
+        GamePadCapabilities mcapabilities;
+        PlayerIndex mPlayerIndex;
+        GamePadState mState;
         public PlayerController(int PlayerNumber)
         {
             
             switch (PlayerNumber)
             {
                 case 1:
-                    capabilities = GamePad.GetCapabilities(Microsoft.Xna.Framework.PlayerIndex.One);
+                    mcapabilities = GamePad.GetCapabilities(Microsoft.Xna.Framework.PlayerIndex.One);
+                    mPlayerIndex = PlayerIndex.One;
                     break;
                 case 2:
-                    capabilities = GamePad.GetCapabilities(Microsoft.Xna.Framework.PlayerIndex.Two);
+                    mcapabilities = GamePad.GetCapabilities(Microsoft.Xna.Framework.PlayerIndex.Two);
+                    mPlayerIndex = PlayerIndex.Two;
                     break;
                 default:
-                    capabilities = GamePad.GetCapabilities(Microsoft.Xna.Framework.PlayerIndex.One);
+                    mcapabilities = GamePad.GetCapabilities(Microsoft.Xna.Framework.PlayerIndex.One);
+                    mPlayerIndex = PlayerIndex.One;
                     break;
             }
             
-            if (!capabilities.IsConnected)
+            if (!mcapabilities.IsConnected)
             {
                 throw new Exception("Controller Not connected, derp");
             }
+            
         }
-        public void Update()
+       
+        public bool dPadUpisDown()
         {
-
+            mState = GamePad.GetState(mPlayerIndex);
+            if(mState.DPad.Up == ButtonState.Pressed)
+            {
+                return true;
+            }
+            return false;
         }
-
+        public bool dPadDownisDown()
+        {
+            mState = GamePad.GetState(mPlayerIndex);
+            if (mState.DPad.Down == ButtonState.Pressed)
+            {
+                return true;
+            }
+            return false;
+        }
+        public bool dPadLeftisDown()
+        {
+            mState = GamePad.GetState(mPlayerIndex);
+            if (mState.DPad.Left == ButtonState.Pressed)
+            {
+                return true;
+            }
+            return false;
+        }
+        public bool dPadRightisDown()
+        {
+            mState = GamePad.GetState(mPlayerIndex);
+            if (mState.DPad.Right == ButtonState.Pressed)
+            {
+                return true;
+            }
+            return false;
+        }
+        public bool AisDown()
+        {
+            mState = GamePad.GetState(mPlayerIndex);
+            if (mState.Buttons.A == ButtonState.Pressed)
+            {
+                return true;
+            }
+            return false;
+        }
+        public bool BisDown()
+        {
+            mState = GamePad.GetState(mPlayerIndex);
+            if (mState.Buttons.B == ButtonState.Pressed)
+            {
+                return true;
+            }
+            return false;
+        }
+        public bool YisDown()
+        {
+            mState = GamePad.GetState(mPlayerIndex);
+            if (mState.Buttons.Y == ButtonState.Pressed)
+            {
+                return true;
+            }
+            return false;
+        }
+        public bool XisDown()
+        {
+            mState = GamePad.GetState(mPlayerIndex);
+            if (mState.Buttons.X == ButtonState.Pressed)
+            {
+                return true;
+            }
+            return false;
+        }
+        public bool RightSholderisDown()
+        {
+            mState = GamePad.GetState(mPlayerIndex);
+            if (mState.Buttons.RightShoulder == ButtonState.Pressed)
+            {
+                return true;
+            }
+            return false;
+        }
+        public bool LeftSholderisDown()
+        {
+            mState = GamePad.GetState(mPlayerIndex);
+            if (mState.Buttons.LeftShoulder == ButtonState.Pressed)
+            {
+                return true;
+            }
+            return false;
+        }
+        public bool StartisDown()
+        {
+            mState = GamePad.GetState(mPlayerIndex);
+            if (mState.Buttons.Start == ButtonState.Pressed)
+            {
+                return true;
+            }
+            return false;
+        }
+        public bool SelectisDown()
+        {
+            mState = GamePad.GetState(mPlayerIndex);
+            if (mState.Buttons.Back == ButtonState.Pressed)
+            {
+                return true;
+            }
+            return false;
+        }
     }
 }

a47dbadf070c6c63190b54e86422bc3e3e464ba4
Gorgonx7 <james.gordon1628@gmail.com>
2017-11-03 18:10:03 +0000

    uploaded branch

---
+++ Game2/Helpers/Menu.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System.Diagnostics;
+using System.Collections.Generic;
+using System;
+using Microsoft.Xna.Framework.Media;
+
+namespace Game2.Helpers
+{
+    public class Menu
+    {
+        public event EventHandler OnMenuChange;
+
+        public string mAxis, mEffects;
+        [XmlElement("Item")]
+        public List<MenuItem> mItems;
+
+        public void LoadContent()
+        {
+
+        }
+
+        public void UnloadContent()
+        {
+
+        }
+
+        public void Update(GameTime gameTime)
+        {
+
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+
+        }
+    }
+}

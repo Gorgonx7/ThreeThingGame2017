94a4b22c8ca5c82cafdf22cc7cdaca57054cb387
Gorgonx7 <james.gordon1628@gmail.com>
2017-11-03 18:34:50 +0000

    players

--- Game2/Player.cs
+++ Game2/Player.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Graphics;
 namespace Game2
 {
     class Player
@@ -12,9 +13,23 @@ namespace Game2
         PlayerController mControler;
         private Vector2 mPosition;
         private Vector2 mVelocity;
-        public Player(int playerNumber)
+        private Texture2D mTexture;
+        private Rectangle mRectange;
+        private float mHealth = 100f;
+        private int playerNumber;
+        public Player(int pplayerNumber)
         {
-            mControler = new PlayerController(playerNumber);
+            playerNumber = pplayerNumber;
+            mTexture = TextureDictionary.FindTexture("player");
+            mControler = new PlayerController(pplayerNumber);
+        }
+        public void setPosition(Vector2 pVector)
+        {
+            mPosition = pVector;
+        }
+        public void Draw(SpriteBatch pSpriteBatch)
+        {
+            pSpriteBatch.Draw(mTexture, mRectange, Color.White);
         }
         public void Update()
         {
